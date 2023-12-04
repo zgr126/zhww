34de916a5375a8f2c02df463c5c4d3fc40b55725
zgr126 <zgr126@126.com>
2023-12-04 14:35:18 +0800

    r

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,5 +1,18 @@
 <script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router';
+const route = useRouter()
+let toList = (str) => {
 
+  console.log(route)
+  route.push({ name: 'wwLst', query: { searchVal: str } })
+}
+let inputV = ref('')
+let search = e => {
+  let value = inputV.value.trim()
+  if (value === '') return
+  toList(value)
+}
 </script>
 
 <template>
@@ -10,71 +23,75 @@
     </div>
     <div class="mainbtns">
       <input v-model="inputV" />
-      <button class="blackBtn">搜索</button>
+      <button class="blackBtn" @click="search">搜索</button>
     </div>
-    <div  class="mainbtns">
-      <button class="imgbtn redBtn">
+    <div class="mainbtns">
+      <button class="imgbtn redBtn" @click="toList('热门')">
         <img src="/img/hot.png">
         <p>热门石膏</p>
       </button>
-      <button class="imgbtn blackBtn">
+      <button class="imgbtn blackBtn" @click="toList('经典')">
         <img src="/img/mac.png">
         <p>经典石膏</p>
       </button>
     </div>
-    <div  class="mainbtns">
-      <button class="redBtn">
+    <div class="mainbtns">
+      <button class="redBtn" @click="toList('mini')">
         <p>mini</p>
         <p>石膏</p>
       </button>
-      <button class="blackBtn">
+      <button class="blackBtn" @click="toList('特大')">
         <p>特大</p>
         <p>石膏</p>
       </button>
-      <button>
+      <button class="blackFont" @click="toList('其他')">
         <p>其他</p>
         <p>石膏</p>
       </button>
     </div>
   </div>
-  
 </template>
 
 <style scoped lang="stylus">
 $defaultPadding = 5px
-.mainbtns
-  margin 0 $defaultPadding
-  display flex
-  button,input
-    border-radius 5px
-    border 0
-    padding 10px
-    background #fff
-    margin $defaultPadding
-  button
-    font-size 2em
-    flex 1
-    p
-      margin 0
-  .blackBtn
-    background #000
-    color #fff
-  .redBtn
-    color $main_color
-  input 
-    flex auto
-  .imgbtn
-    font-size 2.5em
-    img
-      // height 30vw
-      width 40vw
 .body
   display flex
   flex-direction: column
   margin-bottom: $defaultPadding
-img
-  width 100%
-  background $main_color
+  position absolute
+  left 0
+  bottom 0
+  img
+    width 100%
+  .mainbtns
+    margin 0 $defaultPadding
+    display flex
+    button,input
+      border-radius 5px
+      border 0
+      padding 10px
+      background #fff
+      margin $defaultPadding
+    button
+      font-size 2em
+      flex 1
+      p
+        margin 0
+    .blackBtn
+      background #000
+      color #fff
+    .redBtn
+      color $main_color
+    input 
+      flex auto
+    .imgbtn
+      font-size 2.5em
+      img
+        // height 30vw
+        width 40vw
+
+
+  // background $main_color
 .logo 
   position absolute
   left 1em
--- src/pages/wwLst.vue
+++ src/pages/wwLst.vue
@@ -1,22 +1,119 @@
 <script setup>
-
+import { ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+let route = useRoute()
+let router = useRouter()
+let wwLst = ref([
+  { id: 0, name: '史迪仔', url: '/img/itembk.png' },
+  { id: 0, name: '史迪仔', url: '/img/itembk.png' },
+  { id: 0, name: '史迪仔', url: '/img/itembk.png' },
+  { id: 0, name: '史迪仔', url: '/img/itembk.png' },
+  { id: 0, name: '史迪仔', url: '/img/itembk.png' },
+  { id: 0, name: '史迪仔', url: '/img/itembk.png' },
+  { id: 0, name: '史迪仔', url: '/img/itembk.png' },
+  { id: 0, name: '史迪仔', url: '/img/itembk.png' },
+  { id: 0, name: '史迪仔', url: '/img/itembk.png' },
+])
+console.log(route)
+console.log(route.query.searchVal)
+let searchVal = ref(route.query.searchVal || wwLst.value[0].name)
+let quikSearchLst = ref(['史迪仔', '库洛米', '皮卡丘', '史迪仔', '库洛米', '皮卡丘'])
+// 测试弹框
+let detailsItem = ref(null)
+let showDetails = e => {
+  console.log(e)
+  detailsItem.value = e
+}
+let closeDetails = e => {
+  detailsItem.value = null
+}
+let back = e => {
+  router.push({ name: 'home' })
+}
 </script>
 
 <template>
-  <div>wws</div>
+  <div id="navBtn">
+    <button class="blackBtn" @click="back">返回</button>
+    <button>{{ searchVal }}</button>
+  </div>
+  <div id="body">
+    <div id="quicksearchbar">
+      <button v-for="(item, index) of quikSearchLst">
+        <span>{{ item }}</span>
+      </button>
+    </div>
+    <div id="wwBox">
+      <button v-for="(item, index) of wwLst" @click="showDetails(item)">
+        <img :src="item.url">
+      </button>
+    </div>
+    <div id="details" v-if="detailsItem" @click="closeDetails">
+      <img :src="detailsItem.url" />
+      <div id="handlebar">
+        <img src="/img/share.png" />
+        <img src="/img/like.png" />
+        <span>{{ detailsItem.name }}</span>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style scoped lang="stylus">
+#body 
+  position relative
+  #details
+    position absolute
+    top 0
+    left 0
+    height 100%
+    width 100%
+    z-index 1
+    background #00000085
+    img
+    
+      width 100%
+    #handlebar
+      text-align: left;
+      line-height 4em
+      img 
+        vertical-align: bottom;
+        height 4em
+        width 4em
+        border-radius 5px
+        margin 1em
+      span
+        color #fff
+        font-size 3em
+  #wwBox
+    padding 1.5vw
+    overflow auto
+    button
+      border-radius 5px
+      width 45vw
+      height 45vw
+      margin 1.5vw
+      img
+        height 90%
+        width 90%
+        object-fit cover
+        border-radius 5px
+  #quicksearchbar
+    min-height 4em
+    background #D9D9D9
+    overflow-x auto
+    overflow-y hidden
+    white-space: nowrap;
+    button
+      list-style none
+      height 3em
+      margin .5em
+      display inline-block
+      background #fff
+      border-radius 5px
+      padding 0 10px
+      // line-height 3em
+      span
+        font-size 2em
+
 </style>
